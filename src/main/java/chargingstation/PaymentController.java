2cec0e3638d86be8da7902a99c2efa1e4115c7b4
alex joung <nkjdj@naver.com>
2021-09-28 19:51:55 +0900

    Update PaymentController.java

--- src/main/java/chargingstation/PaymentController.java
+++ src/main/java/chargingstation/PaymentController.java
@@ -28,13 +28,13 @@ public class PaymentController {
 
 
         return true;
-        /*
+        
         // CB test 용 지연 코드.
         try {
         Thread.currentThread().sleep((long) (400 + Math.random() * 220));
         } catch (InterruptedException e) {
         e.printStackTrace();
         }
-        */
+        
     }
 }
